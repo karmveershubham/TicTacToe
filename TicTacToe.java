0c33610a1ee0e3d206a8dcbf3d962a0a2ee53c68
karmveershubham <s.karmveer1201@gmail.com>
2024-04-12 15:55:49 +0530

    updating board.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
@@ -5,25 +6,42 @@ public class TicTacToe {
     private Player player1;
     private Player player2;
     private Player currentPlayer;
+    boolean againstComputer=false;;
 
-    public TicTacToe() {
+    public TicTacToe(int vsComputer,char symbol) {
         board = new Board();
-        player1 = new Player('O');
-        player2 = new Player('X');
+        if(symbol=='O' ||symbol=='o'){
+            player1 = new Player('O');
+            player2 = new Player('X'); 
+        }else{
+            player1 = new Player('X');
+            player2 = new Player('O'); 
+        }
+        if (vsComputer==2) {
+           againstComputer=true;
+        }
         currentPlayer = player1;
     }
 
     public void playGame() {
         Scanner scanner = new Scanner(System.in);
         int row, col;
-        
         while (true) {
             System.out.println("Current Board:");
             board.displayBoard();
-            System.out.println(currentPlayer.getSymbol() + "'s turn.");
-            System.out.print("Enter row and column (0-2): ");
-            row = scanner.nextInt();
-            col = scanner.nextInt();
+            if (againstComputer && currentPlayer == player2) {
+                System.out.println(currentPlayer.getSymbol() + "'s turn.\n");
+                Random rand = new Random();
+                do {
+                    row = rand.nextInt(3);
+                    col = rand.nextInt(3);
+                } while (!board.isEmptyCell(row, col));
+            }else {
+                System.out.println(currentPlayer.getSymbol() + "'s turn.");
+                System.out.print("Enter row and column (0-2): ");
+                row = scanner.nextInt();
+                col = scanner.nextInt();
+            }
             if (board.makeMove(row, col, currentPlayer.getSymbol())) {
                 if (board.checkWin(currentPlayer.getSymbol())) {
                     System.out.println("Current Board:");
@@ -45,8 +63,13 @@ public class TicTacToe {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         char choice='y';
-        while(choice=='y'){
-            TicTacToe game = new TicTacToe();
+        while(choice=='y' || choice=='Y'){
+            System.out.println("\n------------TIC TAC TOE------------\n");
+            System.out.println("Choose Mode:  1.Play with Friends  2.play Against Computer.");
+            int vsComputer=scanner.nextInt();
+            System.out.print("Choose Symbol: (X/O) \t ");
+            char symbol=scanner.next().charAt(0);
+            TicTacToe game = new TicTacToe(vsComputer,symbol);
             game.playGame();
             System.out.println("\nDo you want to play Again?(y/n): ");
             choice=scanner.next().charAt(0);
